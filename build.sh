725f68f0b6518830088e156f2f75a5eae8f2f0f0
ShyanJMC <shyanjmc@protonmail.com>
2021-12-03 05:17:29 -0300

    Update

--- build.sh
+++ build.sh
@@ -2,11 +2,15 @@
 
 MODE=$(grep "march" /etc/makepkg.conf | cut -d'=' -f3 | cut -d' ' -f1)
 
-if [ $1 -e "--help" ] || [ $1 -z ];
+if [ "$1" = "--help" ];
 then
 echo -e "For now there is support for GCC. Clang will be unser support in the future. 
 native = in specific for your CPU 
-x86-64 = a generic 64bits CPU 
+64b = a generic 64bits x86-64 CPU
+
+Until GCC 12.1 the ARM native support for ARMv9 will not under support.
+For now ARM support is not working, will be in the future.
+
 armv8-a = Armv8-A 
 armv8.1-a = Armv8.1-A 
 armv8.2-a = Armv8.2-A 
@@ -19,13 +23,13 @@ armv9-a = Armv9-A
 armv8-r = Armv8-R "
 fi
 
-if [ $1 -e "native" ];
+if [ "$1" = "native" ];
 then
-	sed -i "s/march\=$MODE/march\=native/g" /etc/makepkg.conf
+        sed -i "s/march\=$MODE/march\=native/g" /etc/makepkg.conf
 fi
 
-if [ $1 -e "x86-64" ];
+if [ "$1" = "64b" ];
 then
-	sed -i "s/march\=$MODE/march\=x86-64/g" /etc/makepkg.conf
+        sed -i "s/march\=$MODE/march\=x86-64/g" /etc/makepkg.conf
 fi
 
